05ab9be296e33328d7fd17a90136d73908f4a827
Master449 <20436145+Master449@users.noreply.github.com>
2025-02-26 15:00:06 -0600

    Thanks Bolt for the help with fixing inconsistencies

--- flake.nix
+++ flake.nix
@@ -4,9 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
-    stylix.url = "github:danth/stylix";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs = { ... }@inputs: 
--- packages/all.nix
+++ packages/all.nix
@@ -9,6 +9,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    obsidian
     base16-schemes
     discord
     cifs-utils
